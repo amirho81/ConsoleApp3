f07499440bee336ccb7fd4ff73b268b0cd80f0d0
AsusIran <AsusIran@AMIRHAMED>
2023-11-01 14:16:45 +0330

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class TriangularSequence
+{
+    static void Main()
+    {
+        Console.Write("یک عدد وارد نمایید :");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(triangularSequence(n));
+    }
+    static string triangularSequence (int n)
+    {
+        string sequence = " ";
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+            sequence += sum + ", ";
+        }
+        sequence = sequence.Remove(sequence.Length - 2);
+        return sequence;
+    }
+}
+//امیرحسین جورابچی
+//6.دنباله مثلثی
